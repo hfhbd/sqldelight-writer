12453d5f895fb23edd3900531ae7c17092b855c8
hfhbd <hfhbd@users.noreply.github.com>
2023-07-06 10:20:15 +0200

    CI: Add build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,20 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+plugins {
+    id("com.gradle.enterprise") version "3.13.4"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        if (System.getenv("CI") != null) {
+            publishAlways()
+            tag("CI")
+        }
+    }
+}
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
